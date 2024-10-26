63ebaabf6d42933737cff802c4ed317b21ef945a
maksimfedorov <fedorov-max89@mail.ru>
2024-10-26 18:51:18 +0300

    INIT

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-coursework-two
-курсовая работа 2
---
+++ src/test/java/pro/sky/mlfedorov/java_coursework_two/JavaCourseworkTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mlfedorov.java_coursework_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseworkTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
