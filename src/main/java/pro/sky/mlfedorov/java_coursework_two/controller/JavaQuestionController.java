3dd710b14a17e2d72f9da0563d0554aaa4ca0fb7
maksimfedorov <fedorov-max89@mail.ru>
2024-10-26 23:14:20 +0300

    [lesson19489]: создать ExamController

--- src/main/java/pro/sky/mlfedorov/java_coursework_two/controller/JavaQuestionController.java
+++ src/main/java/pro/sky/mlfedorov/java_coursework_two/controller/JavaQuestionController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.mlfedorov.java_coursework_two.entity.Question;
-import pro.sky.mlfedorov.java_coursework_two.service.JavaQuestionServiceImpl;
 import pro.sky.mlfedorov.java_coursework_two.service.QuestionService;
 
 @RestController
