896d69253baadcc2be15ab4d482fc2850d528349
maksimfedorov <fedorov-max89@mail.ru>
2024-10-26 19:21:01 +0300

    [lesson19489]: создать QuestionService

---
+++ src/main/java/pro/sky/mlfedorov/java_coursework_two/service/QuestionService.java
@@ -0,0 +1,21 @@
+package pro.sky.mlfedorov.java_coursework_two.service;
+
+import pro.sky.mlfedorov.java_coursework_two.entity.Question;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public interface QuestionService {
+    final Set<Question> questions = new HashSet<>();
+
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
